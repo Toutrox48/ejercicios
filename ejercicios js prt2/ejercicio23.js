18379f7c419dec477ebe438e2da7250e178848c8
santiago <santiagovillacano48@gmail.com>
2025-02-23 07:52:48 -0500

    primer comit

---
+++ ejercicios js prt2/ejercicio23.js
@@ -0,0 +1,9 @@
+let modelo = Number(prompt('digite el numero de modelo de su carro'))
+let carm = [119, 179, 189, 195, 221, 780]
+
+
+if(modelo != [119, 179, 189, 195, 221, 780]){
+    alert('su modelo de carro no es defectuoso')
+}else{
+    alert('su modelo de carro es defectuoso')
+}
