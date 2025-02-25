85409a63beff02c064c42f6f904bbbda3f4114ca
santiago <santiagovillacano48@gmail.com>
2025-02-25 18:43:35 -0500

    primer comit

--- ejercicios js/unificacion.js
+++ ejercicios js/unificacion.js
@@ -112,7 +112,246 @@ switch (i) {
         dia === 'miercoles' ? alert(`la clase de hoy es ${clase}`) :
             alert(`no se encuentra ningun dato agendado`)
         break;
-                                                            
+            
+    case 12:
+        let usuario = prompt('ingrese su nombre');
+        let horas = Number(prompt('ingrese el numero de horas'));
+
+        let salario = Math.floor(horas * 30000);
+
+        if (numerohoras <= 10) {
+            alert(`Señor ${usuario}, la cantidad de horas trabajadas son ${horas}, por lo tanto su salario es ${salario}`);
+        } else if (numerohoras >= 11) {
+            salario += horas * 3000; 
+            alert(`Señor ${usuario}, la cantidad de horas trabajadas son ${horas}, por lo tanto su salario es ${salario}`);
+        }else{
+            alert(`señor(a) ${usuario}, la cantidad de horas trabajadas son ${horas}, por lo tanto su ${salario}`)
+        }
+        break;
+    
+    case 13:
+        let num = Number(prompt('digite un numero señor(a) usuario '))
+        let resultado = num % 2
+
+        if (resultado === 0){
+            alert(`el numero que digito es par`)
+        }else if(resultado === 1){
+            alert(`el numero ingresado es impar`)
+        }
+        break;    
+    
+    case 14:
+        let tipolavadora = Number(prompt('selecione el tipo de lavadora que desea utilizar tipo 1 o 2'))
+        let numerodelavadoras = Number(prompt('digite la cantidad de lavadoras alquiladas'))
+        let h = Number(prompt('digite la cantidad de horas de alquiler '))
+
+        let lavadora1 = 4000
+        let lavadora2 = 3000
+        let total = 0
+
+        if( tipolavadora === 1 ){
+            total += lavadora1 * h * numerodelavadoras
+            alert( `el costo total por alquilar ${numerodelavadoras} lavadoras de tipo ${tipolavadora} por ${horas} horas : es de ${total}`  )
+            }else if (tipolavadora === 2){
+                total += lavadora2 * h * numerodelavadoras
+                alert( `el costo total por alquilar ${numerodelavadoras} lavadoras de tipo ${tipolavadora} por ${horas} horas : es de ${total}`  )
+            }else{
+                alert('ERROR.......... vuelve a intentarlo')
+            }
+        if (numerodelavadoras >= 3){
+            let descuento = total * 0.03
+            total -= descuento
+            alert(`con el descuento del 3% usted debe de pagar un total de ${total}`)
+        }
+        break;
+        
+    case 15:
+        let n = Number(prompt("Por favor ingresa un número:"))
+
+        if (n % 2 === 0) {
+            alert(`El número ${n} es par.`)
+        } else {
+            alert(`El número ${n} es Impar.`)
+        }
+        break;
+
+    case 16:
+        let  N1 = Number(prompt('digite su nota en el area de la Fisica'))
+        let  N2 = Number(prompt('digite su nota en el area de la Quimica'))
+        let  N3 = Number(prompt('digite su nota en el area de la Biologia'))
+        let  N4 = Number(prompt('digite su nota en el area de la Matematicas'))
+        let  N5 = Number(prompt('digite su nota en el area de la Informática'))
+
+        let notaF = ((N1 + N2 + N3 + N4 + N5) /50) * 100
+
+
+        if(notaF >= 80  ){
+            alert(`su nota  es ${notaF}, y su calificacion cualitativa es superior`)
+            }else if(79<= notaF, notaF >= 60){
+                alert(`su nota  es ${notaF}, y su calificacion cualitativa es buena `)
+            }
+        else{
+            alert(`su nota  es ${notaF}, y su calificacion cualitativa es mala`)
+            }
+        break;
+
+    case 17:
+        let num1 = Number(prompt('ingrese un numero '))
+        let num2 = Number(prompt('ingrese el segundo numero'))
+        let num3 = Number(prompt('ingrese el tercer numero'))
+
+
+        if( num1> num2 , num1 > num3 ){
+            alert('el primer numero es mayor al resto')
+        }
+        else if( num2> num1 , num2 > num3){
+            alert('el segundo numero es mayor')
+        }
+        else if( num3 > num1 , num3 > num2){
+            alert('el tercer numero es mayor')
+        }else{
+            alert('los tres numero son iguales ')
+        }    
+        break;
+
+    case 18:
+        let g = prompt('señor(@) usuario ingrese su genero, si es Hombre  o  si es Mujer ')
+        let edad = Number(prompt('digite su edad señor(@) usuario'))
+
+
+        if(g === 'mujer' , edad >= 50){
+            alert('señora usuaria usted Reciben, $120,000.')
+        }else if (g === 'mujer' , edad <= 50 , edad >= 30){
+            alert('señora usuaria usted Reciben, $100,000.')
+        }else if(g === 'mujer' , edad < 30){ 
+            alert('señora usuaria usted no recibe ayuda.')
+        }
+
+        Function (seletgenero = 'hombre' )
+            seletgenero==='hombre'
+            alert('todo señor usuario Reciben $40,000 (sin importar la edad).')
+        break;
+
+    case 19:
+        let mensualidad = Number(prompt('ingrese su tipo de mensualidad sabiendo que son de; 15, 30, 3 meses'))
+        if(mensualidad === 15){
+            alert('su valor a pagar por la mensualidad de 15 dias es de 18,000.')
+        }else if(mensualidad === 30){
+            alert('su valor a pagar por la mensualidad de 30 dias es de 35,000.')
+        }else if(mensualidad=== 3){
+            alert('su valor a pagar por la mensualidad de 3 meses es de 86,000.')
+        }else{
+            alert('no se encuentra ninguna mensualidad con los datos ingresados')
+        }
+        break;
+
+    case 20:
+        let lado1 = Number(prompt('ingrese el primer angulo'))
+        let lado2 = Number(prompt('ingrese el segundo angulo'))
+        let lado3 = Number(prompt('ingrese el tercer angulo'))
+
+        let triangulo = lado1 +lado2 + lado3
+
+        if ( triangulo === 180){
+        alert('este es un triangulo valido') 
+        }else{
+            alert('el triangulo no es valido')
+        }
+        break;
+
+    case 21:
+        let copias = Number(prompt('ingrese la cantidad de copias para saber el valor a pagar.... desde 1 a mas de  1000 copias '))
+        let precio = 0
+        if(copias > 0 , copias <= 499){
+            precio = copias * 120
+            alert(`el valor a pagar por la cantidad de copias es ${precio}`)
+        }else if(copias > 499, copias <= 749){
+            precio = copias * 100
+            alert(`el valor a pagar por la cantidad de copias es ${precio}`)
+        }else if(copias > 749, copias <= 999){
+            precio = copias * 80
+            alert(`el valor a pagar por la cantidad de copias es ${precio}`)
+        }else if(copias >1000){
+            precio = copias * 50
+            alert(`el valor a pagar por la cantidad de copias es ${precio}`)
+        }else{
+            alert('error no hay numero de copias ')
+        }
+        break;
+
+    case 22:
+        let problem1 = prompt('digite si,  su computadora emite un pitido al iniciar y si su disco duro gira, sino emite ninguno digite no ')
+        let problem2 = prompt('digite si,  su computadora emite un pitido  y si la unidad no gira, sino emite ninguno digite no ')
+        let problem3 = prompt('digite si,  su computadora emite un pitido  y si el disco no gira, sino emite ninguno digite no')
+
+
+
+        if(problem1 === 'si'){
+            alert('pongase en contacto con el técnico de apoyo ')
+        }else if(problem2 === 'si'){
+            alert('verificar los contactos de la unidad')
+        }else if(problem3 === 'si'){
+            alert('Traiga la computadora para repararla en la central')      
+        }else{
+            alert('compruebe la conexion de los altavoces')
+        }
+        break;
+
+    case 23:
+        let modelo = Number(prompt('digite el numero de modelo de su carro'))
+
+        if(modelo != [119, 179, 189, 195, 221, 780]){
+            alert('su modelo de carro no es defectuoso')
+        }else{
+            alert('su modelo de carro  es defectuoso')
+        }
+        break;
+
+    case 24:
+        op=prompt("ingrese su operador...  (tigo)-(claro)-(movistar)")
+        min=Number(prompt("digite la cantidad de minutos consumidos internacionalmente"))
+        break;
+
+    case 25:
+        let san = Number(prompt('ingrese 1 si quiere Sandwich grande o 2 si quiere Sandwich pequeño...'))
+        let adi = prompt('desea agregar alguna de las siguentes productos como tocineta, jalapeño, pavo, queso, ninguna  ')
+
+        if( san=== 1 && adi==='ninguna' ){
+            total = 12000 
+            alert('el total a pagar por su sandwich es de ${total}')
+        }else if(san=== 1 && adi ==='tocineta'){
+            total = 12000 + 3000 
+            alert('el total a pagar por su sandwich con adicion de tocineta es de ${total}')
+        }else if(san=== 1 && adi === 'pavo'){
+            total = 12000 + 3000
+            alert('el total a pagar por su sandwich con adicion de pavo  es de ${total}')
+        }else if(san === 1 && adi === 'queso'){
+            total = 12000 + 2500
+            alert('el total a pagar por su sandwich con adicion de queso  es de ${total}')
+        }else if(san === 1 && adi === 'jalapeño'){
+            total = 12000 + 0
+            alert('el total a pagar por su sandwich con adicion de jalapeño es de ${total}')
+        }else if(san === 2 && adi==='ninguna'){
+            total = 6000 
+            alert('el total a pagar por su sandwich es de ${total}')
+        }else if(san === 2 && adi==='tocineta'){
+            total = 6000 + 3000
+            alert('el total a pagar por su sandwich es de ${total}')
+        }else if(san=== 2 && adi==='pavo'){
+            total = 6000 + 3000
+            alert('el total a pagar por su sandwich es de ${total}')
+        }else if(san=== 2 && adi==='queso'){
+            total = 6000 + 2500
+            alert('el total a pagar por su sandwich es de ${total}')
+        }else if(san=== 2 && adi==='jalapeño'){
+            total = 6000 + 0
+            alert('el total a pagar por su sandwich es de ${total}')
+        }else{
+            alert('por favor haga su orden segun las reglas ')
+        }
+        break;
+
     default:
+        alert("El valor no es valido...")
         break;
 }
